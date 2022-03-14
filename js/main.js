2d54144ca818015e5092237ea559c3941866e294
Kholifatul Ardliyan <k.ardliyan@gmail.com>
2022-03-15 00:09:27 +0700

    First commits

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Disable Text Selection
+var cursor = document.getElementsByTagName('body');
+cursor[0].setAttribute('onmousedown', 'return false');
+cursor[1].setAttribute('onselect', 'return false');
