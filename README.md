08edaaf25bf0a5839bbcc6cb8e602551116d064a
Kholifatul Ardliyan <k.ardliyan@gmail.com>
2022-03-21 00:02:40 +0700

    done guys
    add pagination on about section and mini bug-fix

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# landing-page-love-pet
+# Landing Page Love Pet
+
+## Summary
+Pertama-tama ucapan terima kasih kepada [Wahyu Saputra](https://github.com/wa765) untuk izin menggunakan desainnya untuk di konversi ke tampilan web, mantap.
+
+Tampilan web ini dibuat untuk sarana belajar membuat tampilan dari desain yang sudah ada menjadi tampilan jadi menggunakan ilmu yang telah dipelajari dari berbagai sumber. 
+
+Tampilan landing page ini sudah mendukung untuk berbagai tampilan untuk dekstop, tablet, dan mobile sehingga saat nyaman untuk dipandang.
+
+Bila teman-teman yang mampir disini mau berkolaborasi dalam konversi desain ke tampilan website bisalah kita kontak-kontak, hehe.
+
+## UI Landing Page
+
+![Love Pet Web Design](https://cdn.dribbble.com/users/6867673/screenshots/16757696/media/63d3f8af101950d6312c5f55ca1f44c9.png)
+
+## <p align="center"> [Demo Website ![new_tab](https://img.icons8.com/cotton/18/000000/external-link--v1.png)](https://k-ardliyan.github.io/landing-page-love-pet/)
+
+</p>
+
+## Whats Tech?
+
+<p align="center">
+<a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">
+<img src="https://cdn-icons-png.flaticon.com/512/174/174854.png" alt="HTML" height="36px"></a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
+<img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="CSS" height="36px"></a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+<img src="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png" alt="JavaScript" height="36px"></a>
+<a href="https://getbootstrap.com/" target="_blank">
+<img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="Bootstrap 5" height="36px"></a>
+<a href="https://sass-lang.com/" target="_blank">
+<img src="https://cdn-icons-png.flaticon.com/512/5968/5968358.png" alt="SASS" height="36px"></a>
+</p>
+
+## Source
+UI Design
+- [Dribble - Wahyu Saputra](https://dribbble.com/shots/16757696-Love-Pet-Web-Design)
+
+Assets Image
+- [Freepik](https://freepik.com)
+- [Pinterest](https://pinterest.com)
+- [Redububble](https://redbubble.com)
+- [Google Images](https://images.google.com)
+
+Assets Icon
+- [Icon Bootstrap](https://icons.getbootstrap.com/)
+- [Fontawesome](https://fontawesome.com/)
+- [Flaticon](https://flaticon.com)
+
+Assets Code
+- [Stackoverflow](https://stackoverflow.com)
+
+## Closing
+Terima kasih sudah berkunjung ke sini, nantikan karya selanjutnya.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,92 @@
 // Disable Text Selection
-// var cursor = document.getElementsByTagName('body');
-// cursor[0].setAttribute('onmousedown', 'return false');
-// cursor[1].setAttribute('onselect', 'return false');
+var cursor = document.getElementsByTagName('body');
+cursor[0].setAttribute('onselectstart', 'return false');
+
+// Pagination
+var current_page = 1;
+var records_per_page = 1;
+
+var objJson = [{
+    adName: `<p>We are a community of animal lovers, especially cats and dogs, we have the initiative
+                                    to maintain environmental health by helping abandoned animals.
+                                    <br><br>
+                                    In addition we also open charities and adoptions for other animal lovers, we are
+                                    also always willing to accept pets or abandoned animals to be cared for here.
+                                    <br><br>
+                                    Our goal is to make animals feel comfortable and healthy so they won't go extinct.
+                                </p>`
+}, {
+    adName: `<p>We are a community of animal lovers, especially cats and dogs, we have the initiative
+                                    to maintain environmental health by helping abandoned animals.
+                                    <br><br>
+                                    Our goal is to make animals feel comfortable and healthy so they won't go extinct.
+                                    <br><br>
+                                    In addition we also open charities and adoptions for other animal lovers, we are
+                                    also always willing to accept pets or abandoned animals to be cared for here.
+                                </p>`
+}, {
+    adName: `<p>Our goal is to make animals feel comfortable and healthy so they won't go extinct.
+                                    <br><br>
+                                    We are a community of animal lovers, especially cats and dogs, we have the initiative
+                                    to maintain environmental health by helping abandoned animals.
+                                    <br><br>
+                                    In addition we also open charities and adoptions for other animal lovers, we are
+                                    also always willing to accept pets or abandoned animals to be cared for here.
+                                </p>`
+}]; // Can be obtained from another source, such as your objJson variable
+
+function prevPage() {
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage() {
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+
+function numPages() {
+    return Math.ceil(objJson.length / records_per_page);
+}
+
+function changePage(page) {
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    listing_table.innerHTML = "";
+
+    for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
+        listing_table.innerHTML += objJson[i].adName;
+    }
+    page_span.innerHTML = page + " / " + numPages();
+
+    if (page == 1) {
+        btn_prev.style.opacity = 0.4;
+        btn_prev.disabled = true;
+    } else {
+        btn_prev.style.opacity = 1;
+        btn_prev.disabled = false;
+    }
+
+    if (page == numPages()) {
+        btn_next.style.opacity = 0.4;
+        btn_next.disabled = true;
+    } else {
+        btn_next.style.opacity = 1;
+        btn_next.disabled = false;
+    }
+}
+
+window.onload = function () {
+    changePage(1);
+};
